38ff809bb23f482f5db9f10738a1d760a969702d
Yenmy Vo <114707371+itsyenmyvo22@users.noreply.github.com>
2025-01-27 16:58:59 -0800

    finish start page and start first scene

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-import express from 'express';
-var router = express.Router();
+//  YALL CAN IGNORE THIS BC WE PROBABLY WONT GET TO USER STUFF 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// import express from 'express';
+// var router = express.Router();
 
-export default router;
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// export default router;
